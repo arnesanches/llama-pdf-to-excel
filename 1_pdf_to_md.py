f41e92e7e0cebdd3bbcb6d3601b9cb3c367124a6
arnesanches <arneantonio@hotmail.com>
2024-12-02 19:53:39 -0300

    Importar a biblioteca necessária para o processamento de PDFs

--- 1_pdf_to_md.py
+++ 1_pdf_to_md.py
@@ -2,3 +2,5 @@ import os
 
 # Define a chave da API necessária para o uso do LlamaParse
 os.environ["LLAMA_CLOUD_API_KEY"] = "llx-LHEW7Nb8mUU69SHG8tndxYHVfcnueC4yLwjsR560vBBInBXi"
+
+from llama_parse import LlamaParse
