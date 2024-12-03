3b37443485a69e67d23c88c79b1c39d4e75fd6c8
arnesanches <arneantonio@hotmail.com>
2024-12-02 22:51:30 -0300

    Implementar a função que transforma tabelas em Markdown para o formato Excel

--- 2_md_to_excel.py
+++ 2_md_to_excel.py
@@ -1,8 +1,10 @@
-import re
-import pandas as pd
-from io import StringIO
-import os
-from openpyxl import load_workbook
+# Importando as bibliotecas necessárias
+import re  # Operações com expressões regulares
+import pandas as pd  # Manipulação e análise de dados
+from io import StringIO  # Tratamento de strings como arquivos para entrada/saída de dados
+import os # Manipulação de diretórios e arquivos
+from openpyxl import load_workbook  # Manipulação de arquivos Excel
+
 
 # Função para identificar e extrair tabelas em formato Markdown do texto
 def tratar_tabelas_texto(texto):
@@ -10,6 +12,7 @@ def tratar_tabelas_texto(texto):
     regra_busca_regex = re.compile(r'((?:\|.+\|(?:\n|\r))+)', re.MULTILINE)
     tabelas = regra_busca_regex.findall(texto)  # Retorna todas as tabelas encontradas
     return tabelas
+
 
 # Função para ajustar automaticamente a largura das colunas em um arquivo Excel
 def ajustar_colunas_excel(caminho_arquivo):
@@ -28,3 +31,23 @@ def ajustar_colunas_excel(caminho_arquivo):
 
     # Salva as alterações no arquivo Excel
     wb.save(caminho_arquivo)
+    
+
+# Função para transformar tabelas em Markdown em arquivos Excel
+def transformar_markdown_excel(texto, num_pagina):
+    # Extrai as tabelas em formato Markdown do texto
+    lista_texto_tabelas = tratar_tabelas_texto(texto)
+
+    if len(lista_texto_tabelas) > 0:  # Verifica se há tabelas para processar
+        for i, texto_tabela in enumerate(lista_texto_tabelas):
+            # Lê a tabela em Markdown como DataFrame do Pandas
+            tabela = pd.read_csv(StringIO(texto_tabela), sep="|", encoding="utf-8", engine="python")
+            tabela = tabela.dropna(how="all", axis=1)  # Remove colunas vazias
+            tabela = tabela.dropna(how="all", axis=0)  # Remove linhas vazias
+            
+            # Define o caminho para salvar o arquivo Excel
+            caminho_arquivo = f"tabelas/Pagina{num_pagina}Tabela{i+1}.xlsx"
+            tabela.to_excel(caminho_arquivo, index=False)  # Salva o DataFrame como Excel
+
+            # Ajusta as colunas do arquivo Excel gerado
+            ajustar_colunas_excel(caminho_arquivo)
