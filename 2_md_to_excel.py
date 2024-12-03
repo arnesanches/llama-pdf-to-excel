f962e110685ea2d40293831a112afe7e10880765
arnesanches <arneantonio@hotmail.com>
2024-12-02 21:33:54 -0300

    Implementar a função para localizar tabelas no formato Markdown

--- 2_md_to_excel.py
+++ 2_md_to_excel.py
@@ -3,3 +3,10 @@ import pandas as pd
 from io import StringIO
 import os
 from openpyxl import load_workbook
+
+# Função para identificar e extrair tabelas em formato Markdown do texto
+def tratar_tabelas_texto(texto):
+    # Regex para encontrar tabelas no formato Markdown
+    regra_busca_regex = re.compile(r'((?:\|.+\|(?:\n|\r))+)', re.MULTILINE)
+    tabelas = regra_busca_regex.findall(texto)  # Retorna todas as tabelas encontradas
+    return tabelas
